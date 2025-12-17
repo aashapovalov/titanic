e2701199840421f1a1520478c21bbe816fa96f32
Aleksei Shapovalov <aleksei.a.shapovalov@gmail.com>
2025-12-17 13:31:16 +0200

    feat: move assets url in separate file

--- src/config/assets.ts
+++ src/config/assets.ts
@@ -2,49 +2,36 @@ import { Port } from '../types';
 
 // Bird animation frames
 export const BIRD_FRAMES: string[] = [
-    "src/public/images/hero/bird_ph1.png",
-    "src/public/images/hero/bird_ph2.png",
-    "src/public/images/hero/bird_ph3.png",
-    "src/public/images/hero/bird_ph4.png",
-    "src/public/images/hero/bird_ph5.png",
-    "src/public/images/hero/bird_ph6.png",
-    "src/public/images/hero/bird_ph7.png",
-    "src/public/images/hero/bird_ph8.png",
-    "src/public/images/hero/bird_ph9.png",
+    "images/hero/bird_ph1.png",
+    "images/hero/bird_ph2.png",
+    "images/hero/bird_ph3.png",
+    "images/hero/bird_ph4.png",
+    "images/hero/bird_ph5.png",
+    "images/hero/bird_ph6.png",
+    "images/hero/bird_ph7.png",
+    "images/hero/bird_ph8.png",
+    "images/hero/bird_ph9.png",
 ];
 
 // Wave images
 export const WAVE_IMAGES: string[] = [
-    "src/public/images/hero/hero_wave_dark_l.png",
-    "src/public/images/hero/hero_wave_light_l.png",
-    "src/public/images/hero/hero_wave_dark_s.png",
-    "src/public/images/hero/hero_wave_light_s.png"
-];
-
-// Seagull images
-export const SEAGULL_IMAGES: string[] = [
-    "src/public/images/hero/bird_ph1.png",
-    "src/public/images/hero/bird_ph2.png",
-    "src/public/images/hero/bird_ph3.png",
-    "src/public/images/hero/bird_ph4.png",
-    "src/public/images/hero/bird_ph5.png",
-    "src/public/images/hero/bird_ph6.png",
-    "src/public/images/hero/bird_ph7.png",
-    "src/public/images/hero/bird_ph8.png",
-    "src/public/images/hero/bird_ph9.png"
+    "images/hero/hero_wave_dark_l.png",
+    "images/hero/hero_wave_light_l.png",
+    "images/hero/hero_wave_dark_s.png",
+    "images/hero/hero_wave_light_s.png"
 ];
 
 // Audio files
 export const AUDIO = {
-    hero: "src/public/audio/hero_soundtrack.mp3",
-    search: "src/public/audio/search-sound.mp3",
-    resultHope: "src/public/audio/result_hope.mp3",
-    resultSad: "src/public/audio/result_sad.mp3"
+    hero: "audio/hero_soundtrack.mp3",
+    search: "audio/search-sound.mp3",
+    resultHope: "audio/result_hope.mp3",
+    resultSad: "audio/result_sad.mp3"
 };
 
 // Port backgrounds mapping
 export const PORT_BACKGROUNDS: Record<Port, string> = {
-    southampton: 'src/public/images/search/ports/southampton.png',
-    cherbourg: 'src/public/images/search/ports/cherbourg.png',
-    queenstown: 'src/public/images/search/ports/queenstown.png'
+    southampton: 'images/search/ports/southampton.png',
+    cherbourg: 'images/search/ports/cherbourg.png',
+    queenstown: 'images/search/ports/queenstown.png'
 };
